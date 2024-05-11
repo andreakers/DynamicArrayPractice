c166f6afcab1d079bed6859682d4694a44423e61
Andre <akers.andre@yahoo.com>
2024-05-11 14:19:23 -0700

    Resize in preogress

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,44 +1,67 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here 
+    this.capacity = defaultSize
+    this.length = 0
+    this.data = new Array(this.capacity)
+    if (this.length > this.capacity) {
+      this.data.length*= 2
+      this.capacity*= 2
+    }
   }
 
   read(index) {
-
-    // Your code here 
+    return this.data[index]
   }
 
   push(val) {
-
-    // Your code here 
+    this.data[this.length] = val
+    this.length++
   }
 
 
   pop() {
-
-    // Your code here 
+    if (this.length === 0) return undefined
+    let newArr = []
+    let removed = this.data[this.length - 1]
+    for (let i = 0; i < this.length - 1; i++) {
+      newArr[i] = this.data[i]
+    }
+    this.length--
+    this.data = newArr
+    return removed
   }
 
   shift() {
-
-    // Your code here 
+    if (this.length === 0 ) return
+    let ret = this.data[0]
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1]
+    }
+    this.length--
+    return ret
   }
 
   unshift(val) {
-
-    // Your code here 
+    for (let i = this.data.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.length++
+    this.data[0] = val
   }
 
   indexOf(val) {
-
-    // Your code here 
+    for (let el in this.data) {
+      if (this.data[el] === val) {
+        return Number(el)
+      }
+    }
+    return - 1
   }
 
   resize() {
-
-    // Your code here 
+    this.data.length*= 2
+    this.capacity = this.capacity * 2
   }
 
 }
